55b201cf0b8c2503c224bca0e7ec2ffe5874f59e
kamlakar26 <kamlakar.mca@gmail.com>
2024-12-11 17:34:24 +0530

    Initial Commit

---
+++ DemoTestLbgAccount/DemoTestLbgAccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTestLbgAccountApp.swift
+//  DemoTestLbgAccount
+//
+//  Created by Kamlakar Shimpi on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoTestLbgAccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoTestLbgAccountTests/DemoTestLbgAccountTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTestLbgAccountTests.swift
+//  DemoTestLbgAccountTests
+//
+//  Created by Kamlakar Shimpi on 11/12/24.
+//
+
+import Testing
+@testable import DemoTestLbgAccount
+
+struct DemoTestLbgAccountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
